f8c9feb241e1db9094d815a8831837ef1fa025fb
mkg-schl <70281332+mkg-schl@users.noreply.github.com>
2022-01-28 12:05:51 +0800

    01.28.2022 updates

--- DMIS-v1/index.php
+++ DMIS-v1/index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Side Navigation Bar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DMIS | Dashboard</title>
 	<link rel="stylesheet" href="style.css">
 	<script src="https://kit.fontawesome.com/0a3cb2abd3.js" crossorigin="anonymous"></script>
 </head>
@@ -10,22 +11,22 @@
 
 <div class="wrapper">
     <div class="sidebar">
-        <h2>DMIS </h2>
+        <h2>DMIS</h2>
         <ul>
             <li><a href="#"><i class="fas fa-tachometer-alt"></i>Dashboard</a></li>
-            <li><a href="#"><i class="fas fa-tags"></i>Category</a></li>
+            <li><a href="#"><i class="fas fa-tags" ></i>Category</a></li>
             <li><a href="#"><i class="fas fa-boxes"></i>Product</a></li>
             <li><a href="#"><i class="fas fa-chart-pie"></i>Report</a></li>
             <li><a href="#"><i class="fas fa-qrcode"></i>Scan</a></li>
+            <li><a href="#"><i class="fas fa-sign-out-alt"></i></i>Logout</a></li>
         </ul>
     </div>
-    <div class="main_content">
-        <div class="header">Welcome! Have a nice day!</div>
+    <div id="main-content" class="main_content">
+        <div class="header">This is a Header</div>
         <div class="info">
-            <div>lorem ipsum</div>
+            <?php include "sidebar-nav/dashboard.html" ?>
         </div>
     </div>
 </div>
-
 </body>
 </html>
